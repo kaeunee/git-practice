1c58d44101ed8a6b1f6484679f212729ce4df8a9
kaeun <kaeun@kaeunui-MacBookAir.local>
2022-12-01 19:26:51 +0900

    1차

---
+++ Backend/git-test/app.js
@@ -0,0 +1,88 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { DataSource } = require("typeorm");
+
+const app = express();
+const myDataSource = new DataSource({
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/ping", (req, res) => {
+  res.json({ messgae: "pong" });
+});
+
+/*
+[TEST]
+각 브랜치에 맞는 API를 34번 라인부터 작성해주세요.
+feature/signin 브랜치의 경우 app.post('/users/signin', ...)
+feature/signup 브랜치의 경우 app.post('/users/signup', ...)
+*/
+
+app.post("/users/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await myDataSource.query(
+    `
+    SELECT
+      users.id
+    FROM
+      users
+    WHERE
+      users.email = ?
+   `,
+    [email]
+  );
+
+  if (!user) {
+    res.json({ message: "SIGNUP_REQUIRED" });
+  }
+
+  return res.json({ userId: user.id });
+});
+
+app.post("/users/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  return await myDataSource.query(
+    `
+      INSERT INTO
+        users (
+          username,
+          email,
+          password			
+        )
+      VALUES (
+        ?,
+        ?,
+        ?,
+        ?
+      )
+    `,
+    [username, email, password]
+  );
+});
+
+app.listen(PORT, () => {
+  myDataSource
+    .initialize()
+    .then(() => {
+      console.log("DB Connection has been initialized");
+    })
+    .catch(() => {
+      console.log("DB Connection has been failed");
+    });
+
+  console.log(`Listening to request on localhost:${PORT}`);
+});
